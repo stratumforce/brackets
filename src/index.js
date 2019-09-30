9f9a1353403836e8987a6bc8e2f027e09d4fb412
Valentin Evteev <stratumforce@protonmail.com>
2019-09-30 14:23:41 +0300

    feat: implement brackets algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const bracket = str[i];
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (bracket === bracketsConfig[j][0]) {
+                if (bracket === bracketsConfig[j][1]) {
+                    if (stack[stack.length - 1] === bracket) {
+                        stack.pop();
+                        break;
+                    }
+                }
+
+                stack.push(bracket);
+                break;
+            }
+
+            if (bracket === bracketsConfig[j][1]) {
+                if (stack.pop() !== bracketsConfig[j][0]) {
+                    return false;
+                }
+
+                break;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
